3b42cf3d0c3b1d382cbe4cbc7b114a2d885b9f57
ZanyuanYang <59616360+ZanyuanYang@users.noreply.github.com>
2021-06-24 12:20:13 +0800

    UI: UI CATEGORY PAGE

--- frontend/pages/pay/pay.js
+++ frontend/pages/pay/pay.js
@@ -86,6 +86,36 @@ Page({
      //wx.navigateTo({
        //url: '/pages/pay/pay'
      //});
-      
+   },
+
+   submitOrder: function(e){
+    let goods_json = JSON.stringify(
+    [
+        {"order_number":"1996","good_id":3, "good_name": "shrimp", "good_price":1000000,"good_quantity":6, "good_image": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fdpic.tiankong.com%2Fsh%2Fj8%2FQJ8129672480.jpg%3Fx-oss-process%3Dstyle%2Fshow&refer=http%3A%2F%2Fdpic.tiankong.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1622620505&t=e3bbaf5dfa465b8bc6f5bac3f836904e"},
+        {"order_number":"19961996199619961996199619961996","good_id":6, "good_name": "crabcrab", "good_price":123214141,"good_quantity":7, "good_image": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fdpic.tiankong.com%2Fsh%2Fj8%2FQJ8129672480.jpg%3Fx-oss-process%3Dstyle%2Fshow&refer=http%3A%2F%2Fdpic.tiankong.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1622620505&t=e3bbaf5dfa465b8bc6f5bac3f836904e"}
+    ]);
+     wx.request({
+       url: 'http://localhost:8080/order/create',
+       method: "POST",
+       header:{
+        "Content-Type": "application/x-www-form-urlencoded"
+       },
+      //  暂时是假数据（测试）
+       data:{
+        open_id: 4,
+        order_number: "156310351561",
+        access_token: "5156165165165",
+        user_name: "远",
+        user_address: "汕头",
+        user_phone: "1561566516516163",
+        order_total_price: 16516516,
+        order_comment: "氛围我爱疯舞您付款了奶粉了万福乐蛙发你了玩法了",
+        order_status: 0,
+        items: goods_json
+       },
+       success: function(res){
+         console.log("支付成功", res.data);
+       }
+     })
    }
 })
