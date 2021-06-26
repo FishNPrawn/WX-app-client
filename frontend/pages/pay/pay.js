26213425ad2c4c9f8dcfb389e873f599a01f9e07
ZanyuanYang <59616360+ZanyuanYang@users.noreply.github.com>
2021-06-26 11:08:26 +0800

    Optimize: Move all img to CDN

--- frontend/pages/pay/pay.js
+++ frontend/pages/pay/pay.js
@@ -96,8 +96,8 @@ Page({
     
     let goods_json = JSON.stringify(
     [
-        {"order_number":"96698287","good_id":6, "good_name": "虾肉", "good_price":46,"good_quantity":10, "good_image": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fdpic.tiankong.com%2Fsh%2Fj8%2FQJ8129672480.jpg%3Fx-oss-process%3Dstyle%2Fshow&refer=http%3A%2F%2Fdpic.tiankong.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1622620505&t=e3bbaf5dfa465b8bc6f5bac3f836904e"},
-        {"order_number":"96698287","good_id":9, "good_name": "肉丸", "good_price":10,"good_quantity":20, "good_image": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fdpic.tiankong.com%2Fsh%2Fj8%2FQJ8129672480.jpg%3Fx-oss-process%3Dstyle%2Fshow&refer=http%3A%2F%2Fdpic.tiankong.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1622620505&t=e3bbaf5dfa465b8bc6f5bac3f836904e"}
+        {"order_number":"963","good_id":6, "good_name": "鸡肉", "good_price":416,"good_quantity":10, "good_image": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fdpic.tiankong.com%2Fsh%2Fj8%2FQJ8129672480.jpg%3Fx-oss-process%3Dstyle%2Fshow&refer=http%3A%2F%2Fdpic.tiankong.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1622620505&t=e3bbaf5dfa465b8bc6f5bac3f836904e"},
+        {"order_number":"963","good_id":9, "good_name": "牛肉", "good_price":120,"good_quantity":30, "good_image": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fdpic.tiankong.com%2Fsh%2Fj8%2FQJ8129672480.jpg%3Fx-oss-process%3Dstyle%2Fshow&refer=http%3A%2F%2Fdpic.tiankong.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1622620505&t=e3bbaf5dfa465b8bc6f5bac3f836904e"}
     ]);
      wx.request({
        url: 'https://fishnprawn.cn/order/create',
@@ -107,15 +107,15 @@ Page({
        },
       //  暂时是假数据（测试）
        data:{
-        open_id: 5,
-        order_number: "96698287",
-        access_token: "154618454165165",
-        user_name: "小马",
-        user_address: "广州",
-        user_phone: "139222610000",
-        order_total_price: 324,
-        order_comment: "请打包好",
-        order_status: 1,
+        openId: 11,
+        order_number: "963",
+        access_token: "2651568465",
+        user_name: "杨先生",
+        user_address: "海珠",
+        user_phone: "151515616516",
+        order_total_price: 32422,
+        order_comment: "请打包好，兵线好",
+        order_status: 0,
         items: goods_json
        },
        success: function(res){
