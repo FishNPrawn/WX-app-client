753fced614cadc7552e39115ba9eb9632bfe0085
ZanyuanYang <59616360+ZanyuanYang@users.noreply.github.com>
2021-07-27 16:33:20 +0800

    Create: create entry page

---
+++ frontend/pages/entry/entry.js
@@ -0,0 +1,29 @@
+// pages/entry/entry.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function () {
+    setTimeout(function(){
+      wx.switchTab({
+        url: '/pages/index/index',
+      })
+    }, 2000)
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+})
